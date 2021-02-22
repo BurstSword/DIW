42a4fcef4df7332a13e25c1a99fd29b3dd5f725e
Mersprec <miguelespigaresrecio@iessoterohernandez.es>
2021-02-22 15:40:04 +0200

    hfg

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,46 +1,95 @@
-/* $(function(){
-    $(".div1").dblclick(function redimensionarDiv(){
-        var finalWidth = '250px';
-        var finalHeight = '250px';
-        if($(this).css('width')=='250px') {
-            finalWidth = '800px';
-            finalHeight = '70px';
-        }
-        $(".div1").animate({width: finalWidth, height: finalHeight});
-    });
-    
-})
- */
+
 
 /* $(function(){
-    $("td").hover(entraMouse, saleMouse);
-    function entraMouse() {
-        $(this).css("background-color", "#ff0");
-      }
-      
-      function saleMouse() {
-        $(this).css("background-color", "#fff");
-      }
-})
- */
+ $(".div1").click(function(){
+   $(this).hide(1500,function(){
+     $(this).show(1500);
+   });
+ });
+}); */
+
+/*  $(function(){
+   $(".div2").click(function(){
+     $(".div1").toggle(1000);
+   });
+ }); */
+
+/*  $(function(){
+  $(".div2").click(function () {
+    if ( $( ".div1" ).is( ":hidden" ) ) {
+      $( ".div1" ).slideDown(2000);
+    } else {
+      $( ".div1" ).slideUp(200);
+    }
+  });
+}); */
+
+ /* $(function(){
+  $(".div1").click(function(){
+    $(this).fadeIn(2000);
+  });
+});  */
+
 /* $(function(){
-    $("#text1").focus(changeFocus);
-    $("#text2").focus(changeFocus);
-    $("#text1").blur(blurFocus);
-    $("#text2").blur(blurFocus);
-})
+ $(".div1").click(function(){
+   $(this).fadeOut(2000,
+     function(){
+       $(this).fadeIn(2000);
+     });
+ });
+});  */
 
+/* $(function () {
+  $(".div2").click(function () {
+    $(".div1").fadeToggle(2000);
+  })
+});  */
 
+/* $(function () {
+ $(".div2").click(function () {
+   $(".div1").slideToggle(2000);
+ })
+});  */
 
-function changeFocus() {
-  
-  $(this).css("color", "#f00");
-}
-function blurFocus() {
-  
-    $(this).css("color", "#000");
-  }
- */
-  $( function() {
-    $("input").tooltip();
-  } );
+ /* $(function(){
+  $(".div2").click(function () {
+    if ( $( ".div1" ).is( ":hidden" ) ) {
+      $( ".div1" ).slideDown(2000);
+    } else {
+      $( ".div1" ).hide();
+    }
+  });
+});  */ 
+
+/* $(function () {
+  $(".div2").click(function () {
+    $("#textito").text("Holi nuevo");
+  })
+}); */
+
+/* $(function () {
+  $(".div2").click(function () {
+    $("#textito").html("Holi nuevo");
+  })
+}); */
+
+/* $(function () {
+  $(".div2").click(function () {
+    $("#textito").after("<h1>Holi nuevo<h1>");
+  })
+}); */
+
+/* $(function () {
+  $(".div2").click(function () {
+    $("#textito").before("<h1>Holi nuevo<h1>");
+  })
+}); */
+
+$(document).ready(function(){
+  $(".div2").click(function(){
+    $(".div1").slideUp(5000);
+  });
+  $(".div3").click(function(){
+    $(".div1").stop();
+  });
+});
